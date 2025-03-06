e4c5db407307715ec37f581181fbbbe47732cc2f
shwetahasabe2204 <shwetahasabe18@gmail.com>
2025-03-06 11:34:28 -0800

    type details added

--- src/components/PropertyDetails.tsx
+++ src/components/PropertyDetails.tsx
@@ -1,4 +1,25 @@
 import {useParams} from "react-router-dom";
+type propDet={
+    total:string,
+    availble:string,
+    area:string,
+    price:number,
+    
+}
+
+
+type Property ={
+    title:String,
+    Developer:string,
+    Address:string,
+    Tags :Array<string>,
+    image:Array<string>,
+    videpPresentation:string,
+    locality:string,
+    projectAt:string,
+    constructionStage:string,
+    propDet:propDet[],
+  }
 
 const PropertyDetails = () => {
     
